dc976b60c8e50d05b04852a584ba9064e641a4b4
Alexander Trauzzi <atrauzzi@gmail.com>
2014-06-30 11:29:25 -0500

    Initial commit.

---
+++ src/ServiceProvider.php
@@ -0,0 +1,14 @@
+<?php namespace Atrauzzi\LaravelMongodb {
+
+	use Illuminate\Support\ServiceProvider as Base;
+
+
+	class ServiceProvider extends Base {
+
+		public function register() {
+
+		}
+
+	}
+
+}
